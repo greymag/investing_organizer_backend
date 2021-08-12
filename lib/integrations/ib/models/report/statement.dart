f9ae7c118b3aaf33a7d58cb414950bc1de9a062f
greymag <greymag@gmail.com>
2021-08-12 18:09:41 +0300

    Statement fromMap method

--- lib/integrations/ib/models/report/statement.dart
+++ lib/integrations/ib/models/report/statement.dart
@@ -2,11 +2,24 @@ class Statement {
   final String brokerName;
   final String brokerAddress;
   final String title;
+  // TODO: DateRange
   final String period;
+  // TODO: DateTime
   final String whenGenerated;
 
   Statement(this.brokerName, this.brokerAddress, this.title, this.period,
       this.whenGenerated);
+
+  factory Statement.fromMap(Map<String, dynamic> map) {
+    return Statement(
+      map['brokerName'] as String,
+      map['brokerAddress'] as String,
+      map['title'] as String,
+      map['period'] as String,
+      map['whenGenerated'] as String,
+    );
+  }
+
   @override
   String toString() {
     return 'Statement(brokerName: $brokerName, brokerAddress: $brokerAddress, '
