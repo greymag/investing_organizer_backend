8ae14a7d75bace82cc79be72778eaa34a1f5a81a
greymag <greymag@gmail.com>
2021-11-09 18:57:04 +0300

    IB: Parse period of report.

--- lib/integrations/ib/models/report/statement.dart
+++ lib/integrations/ib/models/report/statement.dart
@@ -1,9 +1,11 @@
+import 'package:in_date_range/in_date_range.dart';
+import 'package:in_date_utils/in_date_utils.dart';
+
 class Statement {
   final String brokerName;
   final String brokerAddress;
   final String title;
-  // TODO: DateRange
-  final String period;
+  final DateRange period;
   // TODO: DateTime
   final String whenGenerated;
 
@@ -15,7 +17,7 @@ class Statement {
       map['brokerName'] as String,
       map['brokerAddress'] as String,
       map['title'] as String,
-      map['period'] as String,
+      _parsePeriod(map['period'] as String),
       map['whenGenerated'] as String,
     );
   }
@@ -25,4 +27,45 @@ class Statement {
     return 'Statement(brokerName: $brokerName, brokerAddress: $brokerAddress, '
         'title: $title, period: $period, whenGenerated: $whenGenerated)';
   }
+}
+
+// TODO: tests
+DateRange _parsePeriod(String value) {
+  // October 18, 2021 - October 22, 2021
+  final arr = value.split('-');
+  return DateRange(
+    _parseDate(arr[0].trim()),
+    DateUtils.nextDay(_parseDate(arr[1].trim())),
+  );
+}
+
+DateTime _parseDate(String value) {
+  final arr = value.split(',');
+  final arr2 = arr.first.split(' ');
+
+  final monthStr = arr2[0];
+  final dateStr = arr2[1];
+  final yearStr = arr[1].trim();
+
+  return DateTime(
+      int.parse(yearStr), _parseMonth(monthStr), int.parse(dateStr));
+}
+
+const _months = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december',
+];
+
+int _parseMonth(String value) {
+  return _months.indexOf(value.toLowerCase()) + 1;
 }
